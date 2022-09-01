f8b47d695fb7254fd0ea84d775c60a6e9fa687c9
Samza7 <32360691+MysteriousCtrl@users.noreply.github.com>
2022-09-01 13:09:35 +0300

    Source Code
    This is a source code for program

---
+++ mac_changer.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# This is a program for changing media access control address,
+# at start program will read these first.
+import subprocess
+import optparse
+import re
+
+# When user need's help or gives wrong input this is a function to program,
+# for showing help screen or showing error.
+def get_arguments():
+    parser = optparse.OptionParser()
+    parser.add_option("-i", "--interface", dest="interface", help="Interface to change its MAC address")
+    parser.add_option("-m", "--mac", dest="new_mac", help="New MAC address")
+    (options, arguments) = parser.parse_args()
+    if not options.interface:
+        parser.error("[-] Please specify a interface, use --help for more info")
+    elif not options.new_mac:
+        parser.error("[-] Please specify a new MAC, use --help for more info")
+    return options
+
+# If user gives correct input, this is a function to program for changing user's mac address
+def change_mac(interface, new_mac):
+    print("[+] Changing MAC address for " + interface + " to " + new_mac)
+    subprocess.call(["ifconfig", interface, "down"])
+    subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
+    subprocess.call(["ifconfig", interface, "up"])
+
+# function to show current mac address to user at start
+def get_current_mac(interface):
+    ifconfig_result = subprocess.check_output(["ifconfig", interface])
+    mac_address_search_result = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", str(ifconfig_result))
+    if mac_address_search_result:
+        return mac_address_search_result.group(0)
+    else:
+        print("[-] Could not read MAC address.")
+
+# Program will read this at second and start's the process for help or error
+options = get_arguments()
+current_mac = get_current_mac(options.interface)
+print("Current MAC = " + str(current_mac))
+
+change_mac(options.interface, options.new_mac)
+
+#To show has MAC addresss get changed or not
+current_mac = get_current_mac(options.interface)
+if current_mac == options.new_mac:
+    print("[+] MAC address was successfully changed to " + current_mac)
+else:
+    print("[-] MAC address did not get changed.")
